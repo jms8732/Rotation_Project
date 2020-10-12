ac0ad5071a51ea07de2153f1cd09da6b2060b8b3
jms8732 <jms8732@gmail.com>
2020-10-12 21:26:29 +0900

    2020-10-12

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,14 +11,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -29,8 +32,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     boolean checked = false;
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(receiver, new IntentFilter("com.example.ROTATION_ACTIVITY"));
+
+    }
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            plug.setChecked(false);
+        }
+    };
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("jms8732", "Activity onCreate...");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -46,14 +62,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //화면 전환을 permission
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if(!Settings.System.canWrite(this)) {
-                Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
-                intent.setData(Uri.parse("package:" + this.getPackageName()));
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            //오버레이 권한 설정
+            if(!Settings.canDrawOverlays(this)){
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:"+this.getPackageName()));
                 startActivity(intent);
+
             }
         }
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("jms8732","Activity resume");
     }
 
     @Override
@@ -61,7 +83,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v == plug) {
             SharedPreferences.Editor editor = prefs.edit();
             Intent intent = new Intent(this, RotationService.class);
-
             checked = prefs.getBoolean("status", false);
 
             if (!checked) { //스위치를 키는 경우
@@ -84,5 +105,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        unregisterReceiver(receiver);
     }
 }
